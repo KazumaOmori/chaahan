60825b08132895777306530cfab3cef7483e98f6
masaki-shinkawa <>
2018-10-04 18:43:58 +0900

    vue-routerでルーティングするのでフォルダ構成を変更 ・layoutsが1ページ単位のvue ・presentatorsに分割したコンポーネントを入れていくイメージ

---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="l-main">
+      <Chaahan />
+    </div>
+</template>
+
+<script>
+import Chaahan from '../presentators/Chaahan.vue'
+/* eslint-disable no-cond-assign,camelcase */
+export default {
+  components: {
+    Chaahan
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Chaahan from '@/components/Chaahan'
+import MainLayout from '@/components/layouts/MainLayout'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Chaahan',
-      component: Chaahan
+      name: 'MainLayout',
+      component: MainLayout
     }
   ]
 })
